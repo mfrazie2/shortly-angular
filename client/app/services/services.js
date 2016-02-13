03c8bcbd560626dd946b2cb25ab5dbac7fd038fb
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-12 19:13:04 -0600

    Completed links view and controller. Started on shorten view and controller.

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,19 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(res){
+      return res.data;
+    });
+  };
+  
+  return {
+    getLinks: getLinks
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
